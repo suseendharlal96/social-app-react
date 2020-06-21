6448027d552c9cbdcd2500882491a4cb43757b45
lssuseendharlal <lssuseendharlal@gmail.com>
2020-06-21 10:56:38 +0530

    skeleton changed for mobile view

--- src/components/ScreamSkeleton.js
+++ src/components/ScreamSkeleton.js
@@ -3,11 +3,9 @@ import React, { Fragment } from "react";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 import withStyles from "@material-ui/core/styles/withStyles";
+import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
-
-import NoImg from "../images/blank-profile.png";
 
 const styles = {
   card: {
@@ -19,37 +17,9 @@ const styles = {
     flexDirection: "column",
     padding: 25,
   },
-  cover: {
-    minWidth: 200,
-    objectFit: "cover",
-  },
-  handle: {
-    width: 70,
-    height: 15,
-    marginBottom: 7,
-    backgroundColor: "#40c4ff",
-  },
-  date: {
-    height: 10,
-    width: 100,
-    backgroundColor: "rgba(0,0,0, 0.6)",
-    marginBottom: 10,
-  },
   floatChild: {
     width: "15%",
     float: "left",
-    marginBottom: 10,
-  },
-  fullLine: {
-    height: 15,
-    width: "90%",
-    backgroundColor: "rgba(0,0,0, 0.6)",
-    marginBottom: 10,
-  },
-  halfLine: {
-    height: 15,
-    width: "50%",
-    backgroundColor: "rgba(0,0,0, 0.6)",
     marginBottom: 10,
   },
 };
@@ -59,29 +29,35 @@ const ScreamSkeleton = (props) => {
 
   const content = Array.from({ length: 5 }).map((item, index) => (
     <Card className={classes.card} key={index}>
-      <Skeleton width={115} height={115} circle={true} />
-      <CardContent className={classes.cardContent}>
-        <SkeletonTheme color="#40c4ff">
-          <Skeleton width={100} height={15} />
-        </SkeletonTheme>
-        <SkeletonTheme color="rgba(0,0,0, 0.6)">
-          <Skeleton width={200} height={10} />
-        </SkeletonTheme>
-        <div>
-          <div className={classes.floatChild}>
-            <SkeletonTheme color="red">
-              <Skeleton width={50} height={10} />
-            </SkeletonTheme>
-          </div>
-          <div className={classes.floatChild}>
+      <Grid container spacing={1}>
+        <Grid item sm={2} xs={12}>
+          <Skeleton width={115} height={115} circle={true} />
+        </Grid>
+        <Grid item sm={10} xs={12}>
+          <CardContent className={classes.cardContent}>
             <SkeletonTheme color="#40c4ff">
-              <Skeleton width={50} height={10} />
+              <Skeleton width={100} height={15} />
             </SkeletonTheme>
-          </div>
-        </div>
-        <Skeleton width={475} />
-        <Skeleton width={620} />
-      </CardContent>
+            <SkeletonTheme color="rgba(0,0,0, 0.6)">
+              <Skeleton width={200} height={10} />
+            </SkeletonTheme>
+            <div>
+              <div className={classes.floatChild}>
+                <SkeletonTheme color="red">
+                  <Skeleton width="65%" height={10} />
+                </SkeletonTheme>
+              </div>
+              <div className={classes.floatChild}>
+                <SkeletonTheme color="#40c4ff">
+                  <Skeleton width="65%" height={10} />
+                </SkeletonTheme>
+              </div>
+            </div>
+            <Skeleton width="75%" />
+            <Skeleton width="90%" />
+          </CardContent>
+        </Grid>
+      </Grid>
     </Card>
   ));
 
